d60bdf8deded287f6efe033b59f9d0a182af78aa
CZED0M1 <86611941+CZED0M1@users.noreply.github.com>
2024-01-15 04:11:39 +0100

    moving file chooser

--- app/src/main/java/com/example/bakalar/ImportActivity.kt
+++ app/src/main/java/com/example/bakalar/ImportActivity.kt
@@ -1,12 +1,43 @@
 package com.example.bakalar
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.DocumentsContract
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.testosmroid.R
 
+// Request code for selecting a PDF document.
+const val PICK_FILE = 2
 class ImportActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_import)
+        openFile(null)
+    }
+
+
+
+    private fun openFile(pickerInitialUri: Uri?) {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            //TODO problém tu s application/json geo+json
+            type = "application/octet-stream"
+            putExtra(DocumentsContract.EXTRA_INITIAL_URI+"/Downloads", pickerInitialUri)
+        }
+
+        startActivityForResult(intent, PICK_FILE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            if (data != null) {
+                val selectedFileUri = data.data
+                Log.d("myTag", selectedFileUri.toString());
+            }
+        }
     }
 }
--- app/src/main/java/com/example/bakalar/MainActivity.kt
+++ app/src/main/java/com/example/bakalar/MainActivity.kt
@@ -9,20 +9,10 @@ import android.provider.DocumentsContract
 import android.provider.OpenableColumns
 import android.widget.Button
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.MimeTypeFilter
 import com.example.testosmroid.R
 
 class MainActivity : AppCompatActivity() {
-
-
-    private val requestFileLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                result.data?.data?.let { uri ->
-                    handleSelectedFile(uri)
-                }
-            }
-        }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -30,44 +20,24 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
-
+        openMap()
         openFileChooser()
-    openMap()
     }
+
     private fun openMap() {
-        val openMap= findViewById<Button>(R.id.buttonMapa)
+        val openMap = findViewById<Button>(R.id.buttonMapa)
         openMap.setOnClickListener {
-            val intent = Intent(this,MapsActivity::class.java)
+            val intent = Intent(this, MapsActivity::class.java)
             startActivity(intent)
         }
     }
-    private fun openSpecificFolder() {
-        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
-            addCategory(Intent.CATEGORY_OPENABLE)
-            type = "*/*"  //TODO Set the MIME type to filter files
-            val uri = Uri.parse("content://com.android.externalstorage.documents/document/primary:Documents")
-            putExtra(DocumentsContract.EXTRA_INITIAL_URI, uri)
-        }
-        requestFileLauncher.launch(intent)
-    }
-
-    private fun handleSelectedFile(uri: Uri) {
-        val cursor = contentResolver.query(uri, null, null, null, null)
-        cursor?.use {
-            if (it.moveToFirst()) {
-                val displayName = it.getString(it.getColumnIndexOrThrow(OpenableColumns.DISPLAY_NAME))
-                //TODO tady byl problém napsat do dokumentace - SuppressLint("Range")
-                //https://stackoverflow.com/questions/71338033/android-sqlite-value-must-be-≥-0-getcolumnindex-kotlin?answert
-                println(displayName)
-            }
+    private fun openFileChooser() {
+        val openFile = findViewById<Button>(R.id.buttonImport)
+        openFile.setOnClickListener {
+            val intent = Intent(this, ImportActivity::class.java)
+            startActivity(intent)
         }
     }
 
-   private fun openFileChooser() {
-       val openImport= findViewById<Button>(R.id.buttonImport)
-       openImport.setOnClickListener {
-           openSpecificFolder()
-       }
-    }
 
 }
