c213e3fe26b2da479f759a6d9d23cd19d55977aa
CZED0M1 <86611941+CZED0M1@users.noreply.github.com>
2024-01-18 22:00:14 +0100

    fixing errors (unused imports,...)

--- app/src/main/java/com/example/bakalar/MapsActivity.kt
+++ app/src/main/java/com/example/bakalar/MapsActivity.kt
@@ -11,6 +11,15 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.preference.PreferenceManager
 import com.example.testosmroid.R
+<<<<<<< Updated upstream
+=======
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.OnTokenCanceledListener
+>>>>>>> Stashed changes
 import org.osmdroid.config.Configuration.getInstance
 import org.osmdroid.events.MapEventsReceiver
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
@@ -20,12 +29,35 @@ import org.osmdroid.views.overlay.MapEventsOverlay
 import org.osmdroid.views.overlay.Marker
 import org.osmdroid.views.overlay.Polygon
 import org.osmdroid.views.overlay.gestures.RotationGestureOverlay
+<<<<<<< Updated upstream
 import org.osmdroid.views.overlay.gridlines.LatLonGridlineOverlay2
 
 
 class MapsActivity : AppCompatActivity() {
     private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
     private lateinit var map : MapView
+=======
+
+
+class MapsActivity : AppCompatActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var map: MapView
+    private var isParkClicked: Boolean = false
+    private lateinit var parkingButton: ImageButton
+    private lateinit var cancelButton: ImageButton
+    private lateinit var addButton: ImageButton
+    private lateinit var undoButton: ImageButton
+    private lateinit var rectangle: View
+    private var geoPoints = ArrayList<GeoPoint>()
+    private var markers = ArrayList<Marker>()
+    private val requiredPermissions = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+    private val permissionRequestCode =1001
+//TODO Databáze
+//TODO remove logs
+>>>>>>> Stashed changes
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -162,6 +194,30 @@ class MapsActivity : AppCompatActivity() {
         //Configuration.getInstance().save(this, prefs);
         map.onPause()  //needed for compass, my location overlays, v6.0.0 and up
     }
+<<<<<<< Updated upstream
+=======
+    private fun showPermissionExplanationDialog(context: Context) {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Povolení potřebná pro aplikaci")
+        builder.setMessage("Aplikace potřebuje povolení pro zjištění polohy")
+        builder.setPositiveButton("Povolit") { _, _ ->
+            // Uživatel klikl na Povolit, přejde do nastavení aplikace
+            navigateToAppSettings(context)
+        }
+        builder.setNegativeButton("Zrušit") { dialog, _ ->
+            // Uživatel klikl na Zrušit
+            dialog.dismiss()
+        }
+        builder.setCancelable(false)
+        builder.show()
+    }
+
+    private fun navigateToAppSettings(context: Context) {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        intent.data = android.net.Uri.parse("package:" + context.packageName)
+        context.startActivity(intent)
+    }
+>>>>>>> Stashed changes
 
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -172,11 +228,79 @@ class MapsActivity : AppCompatActivity() {
             permissionsToRequest.add(permissions[i])
             i++
         }
+<<<<<<< Updated upstream
         if (permissionsToRequest.size > 0) {
             ActivityCompat.requestPermissions(
                 this,
                 permissionsToRequest.toTypedArray(),
                 REQUEST_PERMISSIONS_REQUEST_CODE)
+=======
+
+        if (permissionsToRequest.isNotEmpty()) {
+            showPermissionExplanationDialog(this)
+            Log.d("PermissionDenied", "Uživatel klikl na 'nepovolit'")
+            permissionsToRequest.clear()
+        } else {
+            initializeLocation() // Requested permissions granted, initialize the location
+        }
+    }
+
+    private fun initializeLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // Permissions for location are granted, proceed with location actions
+            fusedLocationClient.getCurrentLocation(
+                Priority.PRIORITY_HIGH_ACCURACY,
+                object : CancellationToken() {
+                    override fun onCanceledRequested(p0: OnTokenCanceledListener) = CancellationTokenSource().token
+
+                    override fun isCancellationRequested() = false
+                }
+            ).addOnSuccessListener { location: Location? ->
+                if (location == null) {
+                    Log.d("PermissionDenied", "Poloha null")
+                } else {
+                    Log.d("PermissionDenied", "Poloha not null")
+                    val lat = location.latitude
+                    val lon = location.longitude
+
+                    val actualPosition = GeoPoint(lat, lon)
+
+                    map.controller.setCenter(actualPosition)
+
+                    val marker = Marker(map)
+                    marker.position = actualPosition
+
+                    marker.icon = ContextCompat.getDrawable(
+                        this@MapsActivity,
+                        org.osmdroid.library.R.drawable.person
+                    )
+                    marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+                    map.overlays.add(marker)
+                    map.invalidate()
+                }
+            }
+        }
+    }
+
+    private fun requestPermissionsIfNecessary(permissions: Array<String>) {
+        Log.d("PermissionDenied", "requestPermissionsIfNecessary")
+
+        val permissionsToRequest = ArrayList<String>()
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                permissionsToRequest.add(permission)
+            }
+        }
+
+        if (permissionsToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                this,
+                permissionsToRequest.toTypedArray(),
+                permissionRequestCode
+            )
+        } else {
+            initializeLocation() // Permissions already granted, initialize the location
+>>>>>>> Stashed changes
         }
     }
 
